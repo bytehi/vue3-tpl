fc22a53943a4e28b0dd0af53a940428e08118376
dongli <121712454@163.com>
2024-06-11 21:54:16 +0800

    添加应用模板

---
+++ h5-spa-rem-ts/src/views/login/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts" name="login"></script>
+
+<template>
+  <div>login</div>
+</template>
+
+<style scoped></style>
---
+++ h5-spa-rem-ts/tailwind.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'windicss/helpers'
+
+// https://cn.windicss.org/integrations/vite.html#typeScript
+export default defineConfig({
+  // https://cn.windicss.org/integrations/vite.html#scanning
+  // extract: {
+  //   include: ['src/**/*.{vue,html,jsx,tsx}'],
+  //   exclude: ['node_modules', '.git'],
+  // },
+})
